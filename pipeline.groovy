8dd116d6f2da76449368da424aa01db883026970
capfer27 <capfer27@gmailcom>
2025-12-05 23:59:11 +0400

    copy pipeline out of jenkins web ui into pipeline.groovy file to edit in vscode

---
+++ pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git url: 'https://github.com/capfer27/course3-jenkins-gs-spring-petclinic.git', branch: 'main'
+            }
+        }
+
+        stage ('Build') {
+            steps {
+                sh './mvnw clean package'
+            }
+
+            post {
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+
+}
