beb0a5ae4cb13fda29ad5e713dc1bc4afa5bd14f
capfer27 <capfer27@gmailcom>
2025-12-06 20:57:51 +0400

    Revert "test sending email for broken test"
    This reverts commit 9d8dfccd16a8ddc05752a1625c8813a02784b745.

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("uhhih");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
